c4663777ef1c41fee4ad3ab4863bbcd711c56d82
ZIYAD <97746830+ziyadamz2@users.noreply.github.com>
2023-11-11 16:36:56 +0000

    connection to database

--- project.py
+++ project.py
@@ -3,10 +3,78 @@ import customtkinter
 from tkinter import * 
 from PIL import Image
 import tkinter
-import ttkbootstrap as tb
-from datetime import date
+from typing import Union, Callable
 from ttkbootstrap.dialogs import Querybox
+import pymysql
 customtkinter.set_appearance_mode("light")
+conn = pymysql.connect(host='localhost',user='root',password="",db='project_oop')
+cur = conn.cursor()
+def mysqlconnect(test):
+    cur.execute(test)
+    output = cur.fetchall()
+    return(output)
+
+class FloatSpinbox(customtkinter.CTkFrame):
+    def __init__(self, *args,
+                 width: int = 200,
+                 height: int = 32,
+                 step_size: Union[int, float] = 1,
+                 command: Callable = None,
+                 **kwargs):
+        super().__init__(*args, width=width, height=height, **kwargs)
+
+        self.step_size = step_size
+        self.command = command
+
+        self.configure(fg_color=("gray78", "gray28"))  # set frame color
+
+        self.grid_columnconfigure((0, 2), weight=0)  # buttons don't expand
+        self.grid_columnconfigure(1, weight=1)  # entry expands
+        self.x=True
+        self.subtract_button = customtkinter.CTkButton(self, text="-", width=height-6, height=height-6, command=self.subtract_button_callback,fg_color="#77B5FE")
+        self.subtract_button.grid(row=0, column=0, padx=(3, 0), pady=3)
+
+        self.entry = customtkinter.CTkEntry(self, width=width-2, height=height-6, border_width=0,placeholder_text="number of passengers")
+        self.entry.grid(row=0, column=1, columnspan=1, padx=3, pady=3, sticky="ew")
+
+        self.add_button = customtkinter.CTkButton(self, text="+", width=height-6, height=height-6,command=self.add_button_callback,fg_color="#77B5FE")
+        self.add_button.grid(row=0, column=2, padx=(0, 3), pady=3)
+
+        # default value
+        
+
+
+    def add_button_callback(self):
+        if self.x==True:
+            self.entry.insert(0, "0")
+        self.x=False
+        if self.command is not None:
+            self.command()
+            
+        try:
+            value = int(self.entry.get()) + int(self.step_size)
+            self.entry.delete(0, "end")
+            self.entry.insert(0, value)
+        except ValueError:
+            return
+
+    def subtract_button_callback(self):
+        if self.command is not None:
+            self.command()
+        try:
+            if float(self.entry.get())>0:
+                value = int(self.entry.get()) - int(self.step_size)
+                self.entry.delete(0, "end")
+                self.entry.insert(0, value)
+        except ValueError:
+            return
+
+    def get(self) -> Union[float, None]:
+        try:
+            return float(self.entry.get())
+        except ValueError:
+            return None
+
 
 
 class login_gui:
@@ -29,10 +97,10 @@ class login_gui:
         self.email = customtkinter.CTkEntry(self.frame, placeholder_text="Email",width=250, height=35,font=("cursive",15 ),border_color="#859BF5",border_width=3)
         self.email.place(relx=0.5, rely=0.3, anchor=tkinter.CENTER)
 
-        self.passworld = customtkinter.CTkEntry(self.frame, placeholder_text="Passworld",width=250, height=35,font=("cursive",17 ),border_color="#859BF5",border_width=3)
-        self.passworld.place(relx=0.5, rely=0.45, anchor=tkinter.CENTER)
-
-        self.login=customtkinter.CTkButton(self.frame, text="Login",width=120,fg_color="green",command=lambda:self.logiin())
+        self.password = customtkinter.CTkEntry(self.frame, placeholder_text="Password",width=250, height=35,font=("cursive",17 ),border_color="#859BF5",border_width=3)
+        self.password.place(relx=0.5, rely=0.45, anchor=tkinter.CENTER)
+        
+        self.login=customtkinter.CTkButton(self.frame, text="Login",width=120,fg_color="green",command=lambda:self.Login())
         self.login.place(relx=0.365, rely=0.58, anchor=tkinter.CENTER)
 
         self.creat_an_account=customtkinter.CTkButton(self.frame, text="Creat an account",width=120,fg_color="red",command=lambda:self.creat_an_account())
@@ -42,11 +110,13 @@ class login_gui:
         self.login_as_guest.place(relx=0.5, rely=0.69, anchor=tkinter.CENTER)
 
         
-    def logiin(self):
+    def Login(self):
         
         for widget in self.frame.winfo_children():
             widget.destroy()
         self.frame.destroy()
+        flight=mysqlconnect("select * from member_customer")
+        print(flight)
         bookingapp(self.app)
         
     def logiin_as_guest(self):
@@ -67,48 +137,62 @@ class login_gui:
 class bookingapp:
     def __init__(self,app):
         self.app=app
+
         self.frame=customtkinter.CTkFrame(master=self.app, width=1222, height=200,border_color="#77B5FE",fg_color="white")
         self.frame.place(relx=0.5, rely=0.19, anchor=tkinter.CENTER)
-        airport=["Aéroport Charles de Gaulle | CDG", 
-                "Aéroport Adolfo-Suárez de Madrid-Barajas | MAD",
-                "Aéroport de Francfort | FRA",
-                "Aéroport d'Amsterdam-Schiphol	| AMS",
-                "Aéroport international du Caire | CAI",
-                "Aéroport d'Istanbul | IST" ,
-                "Aéroport international du roi Fahd | JIB"
+        airport=["Paris", 
+                "Madrid",
+                "Franckfort",
+                "Amsterdam",
+                "Caire | CAI",
+                "Istanbul" ,
+                "Djibouti"
                  ]
-        
-        self.arrival_airport = customtkinter.StringVar()
-        self.arrival_airport.set("Select Arrival Airport")
-        self.arrival_airport = customtkinter.CTkComboBox(self.frame,values=airport,border_width=2,variable=self.arrival_airport,width=200,border_color="#77B5FE",button_color="#77B5FE")
-        self.arrival_airport.place(relx=0.3, rely=0.4, anchor=tkinter.CENTER)
+
         
         self.departure_airport = customtkinter.StringVar()
         self.departure_airport.set("Select Departure Airport") 
         self.departure_airport = customtkinter.CTkComboBox(self.frame,values=airport,border_width=2,variable=self.departure_airport,width=200,border_color="#77B5FE",button_color="#77B5FE")
         self.departure_airport.place(relx=0.1, rely=0.4, anchor=tkinter.CENTER)
         
+        self.arrival_airport = customtkinter.StringVar()
+        self.arrival_airport.set("Select Arrival Airport")
+        self.arrival_airport = customtkinter.CTkComboBox(self.frame,values=airport,border_width=2,variable=self.arrival_airport,width=200,border_color="#77B5FE",button_color="#77B5FE")
+        self.arrival_airport.place(relx=0.3, rely=0.4, anchor=tkinter.CENTER)
+                
         self.Class = customtkinter.StringVar()
         self.Class.set("Select Class")
         self.Class = customtkinter.CTkComboBox(self.frame,values=['Economy Class','First Class'],border_width=2,variable=self.Class,width=200,border_color="#77B5FE",button_color="#77B5FE")
         self.Class.place(relx=0.3, rely=0.7, anchor=tkinter.CENTER)
         
-        """"
-        self.my_departue_date = tb.DateEntry(self.frame, bootstyle="danger",firstweekday=0, startdate=date(2023, 2, 14))
-        self.my_departue_date.pack(relx=0.3, rely=0.7, anchor=tkinter.CENTER)
         
-        self.my_arrival_date = tb.DateEntry(self.frame, bootstyle="danger",firstweekday=0, startdate=date(2023, 2, 14))
-        self.my_arrival_date.pack(relx=0.3, rely=0.6, anchor=tkinter.CENTER)
-        """
+        self.my_departure_date =customtkinter.CTkEntry(self.frame, placeholder_text="departure date:yyyy-mm-dd",width=200,font=("cursive",15 ),border_color="#77B5FE",border_width=2)
+        self.my_departure_date.place(relx=0.5, rely=0.4, anchor=tkinter.CENTER)
+        
+        self.my_arrival_date = customtkinter.CTkEntry(self.frame, placeholder_text="arrival date:yyyy-mm-dd",width=200,font=("cursive",15 ),border_color="#77B5FE",border_width=2)
+        self.my_arrival_date.place(relx=0.7, rely=0.4, anchor=tkinter.CENTER)
+                
+  
+        self.spinbox_1 = FloatSpinbox(self.frame, width=140, step_size=1)
+        self.spinbox_1.place(relx=0.1, rely=0.7, anchor=tkinter.CENTER)
+        #print(self.spinbox_1.get())
+        
+        self.login=customtkinter.CTkButton(self.frame, text="SEARCH A FLIGHT",width=200,height=80,fg_color="red",font=("cursive",23 ),command=lambda:self.research())
+        self.login.place(relx=0.89, rely=0.58, anchor=tkinter.CENTER)
+        
         
         self.frame_booking=customtkinter.CTkScrollableFrame(master=self.app, width=1200, height=500,border_color="#77B5FE",fg_color="white")
         self.frame_booking.place(relx=0.5, rely=0.65, anchor=tkinter.CENTER)
         
+    def research(self):
+        flight=mysqlconnect("select * from flight")
         
         
-
-
-
+            
+        
+        
+    
+        
         
          
         
